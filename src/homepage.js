74548be29d6b6ae2ce7deb69a3c7217d84eedf69
Alex Keener <alexkeener318@gmail.com>
2022-07-28 20:57:41 -0500

    Creates homepage for website

--- src/homepage.js
+++ src/homepage.js
@@ -3,14 +3,26 @@ function loadHomepage() {
     const img = document.createElement("img");
     const title = document.createElement("h1");
     const desc = document.createElement("p");
+    const links = document.createElement("div");
+    const homepage = document.createElement("p");
+    const menu = document.createElement("p");
+    const contact = document.createElement("p");
 
     img.setAttribute("src", "../italy.jpg");
     title.textContent = "Italy's Finest Cuisine";
     desc.textContent = "My name is Marco, creator of the tomato, and I have got to say that this is the finest Restaurant in all of the world. Never before have I tasted my creation (the tomato), so finely prepared. 2/10 probably wouldnt eat here again."
+    homepage.textContent = "Homepage";
+    menu.textContent = "Menu";
+    contact.textContent = "Contact Us";
+    links.appendChild(homepage);
+    links.appendChild(menu);
+    links.appendChild(contact);
+    links.classList.add("links");
 
     content.appendChild(img);
     content.appendChild(title);
     content.appendChild(desc);
+    content.appendChild(links);
 }
 
 export default loadHomepage;
